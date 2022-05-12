e781fffcc04f2ff6c1e72c3a2a6db77a08374264
Maria <konstantinovamaria25@gmail.com>
2022-05-12 10:08:17 +0300

    Added 'coverage' to .gitignore. Change 'background' to 'color' in main.js.

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import { sum, getColor } from './module.js';
 
 console.log(sum(4)(3));
 
+
 const text1 = document.getElementById('text1');
 const text2 = document.getElementById('text2');
 const text3 = document.getElementById('text3');
@@ -11,16 +12,16 @@ let count2 = 0;
 let count3 = 0;
 
 text1.addEventListener('click', (event) => {
-  event.target.style.background = getColor(count1);
+  event.target.style.color = getColor(count1);
   count1++;
 });
 
 text2.addEventListener('click', (event) => {
-  event.target.style.background = getColor(count2);
+  event.target.style.color = getColor(count2);
   count2++;
 });
 
 text3.addEventListener('click', (event) => {
-  event.target.style.background = getColor(count3);
+  event.target.style.color = getColor(count3);
   count3++;
 });
