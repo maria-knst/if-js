6a3900e546d8357da71ffe9f190d6446dfd840fa
Maria <konstantinovamaria25@gmail.com>
2022-05-31 14:15:52 +0300

    Made task 2 in homework 7. Added tests to it.

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,46 +1,52 @@
-import { palindrom, getMatchedElements, getCalendarMonth } from '../main';
-
-describe('Check palindrom function', () => {
-  test('Check "adda"', () => {
-    expect(palindrom('adda')).toBe(true);
-  });
-  test('Check "Hello"', () => {
-    expect(palindrom('Hello')).toBe(false);
-  });
-  test('Check ""', () => {
-    expect(palindrom('')).toBe(true);
-  });
-});
-
-describe('Check getMatchedElements-function', () => {
-  test('Check "germany"', () => {
-    expect(getMatchedElements('germany')).toEqual([
-      'Germany, Berlin, Hostel Friendship',
-      'Germany, Hamburg, Steigenberger Hotel',
-    ]);
-  });
-  test('Check "on"', () => {
-    expect(getMatchedElements('on')).toEqual([
-      'Malaysia, Port Dickson, The Andaman Resort',
-      'Indonesia, BTDC, Nuca Dua, Black Penny Villas',
-      'UK, London, Nox Hostel',
-    ]);
-  });
-  test('Check "belarus"', () => {
-    expect(getMatchedElements('belarus')).toEqual([]);
-  });
-  test('Check ""', () => {
-    expect(getMatchedElements('')).toEqual([]);
-  });
-});
+import { getCalendarMonth } from '../main';
 
 describe('Check getCalendarMonth-function', () => {
   const arr1 = [
-    [27, 28, 29, 30, 1, 2, 3],
-    [4, 5, 6, 7, 8, 9, 10],
-    [11, 12, 13, 14, 15, 16, 17],
-    [18, 19, 20, 21, 22, 23, 24],
-    [25, 26, 27, 28, 29, 30, 1],
+    [
+      { daysInMonth: 27, notCurrentMonth: true, selectedDay: false },
+      { daysInMonth: 28, notCurrentMonth: true, selectedDay: false },
+      { daysInMonth: 29, notCurrentMonth: true, selectedDay: false },
+      { daysInMonth: 30, notCurrentMonth: true, selectedDay: false },
+      { daysInMonth: 1, notCurrentMonth: false, selectedDay: false },
+      { daysInMonth: 2, notCurrentMonth: false, selectedDay: false },
+      { daysInMonth: 3, notCurrentMonth: false, selectedDay: false },
+    ],
+    [
+      { daysInMonth: 4, notCurrentMonth: false, selectedDay: false },
+      { daysInMonth: 5, notCurrentMonth: false, selectedDay: false },
+      { daysInMonth: 6, notCurrentMonth: false, selectedDay: false },
+      { daysInMonth: 7, notCurrentMonth: false, selectedDay: false },
+      { daysInMonth: 8, notCurrentMonth: false, selectedDay: false },
+      { daysInMonth: 9, notCurrentMonth: false, selectedDay: false },
+      { daysInMonth: 10, notCurrentMonth: false, selectedDay: false },
+    ],
+    [
+      { daysInMonth: 11, notCurrentMonth: false, selectedDay: false },
+      { daysInMonth: 12, notCurrentMonth: false, selectedDay: false },
+      { daysInMonth: 13, notCurrentMonth: false, selectedDay: false },
+      { daysInMonth: 14, notCurrentMonth: false, selectedDay: false },
+      { daysInMonth: 15, notCurrentMonth: false, selectedDay: false },
+      { daysInMonth: 16, notCurrentMonth: false, selectedDay: false },
+      { daysInMonth: 17, notCurrentMonth: false, selectedDay: false },
+    ],
+    [
+      { daysInMonth: 18, notCurrentMonth: false, selectedDay: false },
+      { daysInMonth: 19, notCurrentMonth: false, selectedDay: false },
+      { daysInMonth: 20, notCurrentMonth: false, selectedDay: false },
+      { daysInMonth: 21, notCurrentMonth: false, selectedDay: false },
+      { daysInMonth: 22, notCurrentMonth: false, selectedDay: false },
+      { daysInMonth: 23, notCurrentMonth: false, selectedDay: false },
+      { daysInMonth: 24, notCurrentMonth: false, selectedDay: false },
+    ],
+    [
+      { daysInMonth: 25, notCurrentMonth: false, selectedDay: false },
+      { daysInMonth: 26, notCurrentMonth: false, selectedDay: false },
+      { daysInMonth: 27, notCurrentMonth: false, selectedDay: false },
+      { daysInMonth: 28, notCurrentMonth: false, selectedDay: false },
+      { daysInMonth: 29, notCurrentMonth: false, selectedDay: false },
+      { daysInMonth: 30, notCurrentMonth: false, selectedDay: false },
+      { daysInMonth: 1, notCurrentMonth: true, selectedDay: false },
+    ],
   ];
   test('Check arr1', () => {
     expect(getCalendarMonth(30, 7, 4)).toEqual(arr1);
