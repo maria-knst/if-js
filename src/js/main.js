922c094bbf49af26178a27a5838fdd2050100dcd
Maria <konstantinovamaria25@gmail.com>
2022-06-16 18:34:42 +0300

    Added increment and decrement functions and added listeners to filters blocks.

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import data from './data_hostels.js';
 
+//HOMES SECTION
 const homesContainer = document.querySelector('.homes__container');
 const homesFlexContainer = homesContainer.querySelector(
   '.places__flex-container',
@@ -25,7 +26,6 @@ data.forEach((element, index) => {
   if (index >= 2) {
     const placesElement =
       homesFlexContainer.querySelectorAll('.places__element')[index];
-    console.log(placesElement);
     placesElement.classList.add('hidden');
   }
   if (index >= 4) {
@@ -33,4 +33,54 @@ data.forEach((element, index) => {
       homesFlexContainer.querySelectorAll('.places__element')[index];
     placesElement.classList.add('temporarily-hidden');
   }
+});
+
+//TOP SECTION
+const filterMembers = ['adult', 'child', 'room'];
+
+const decrement = (event, element1, element2, index) => {
+  event.preventDefault();
+  let num = element1.innerText;
+  const min = index === 1 ? '0' : '1';
+  if (num === min) {
+    return false;
+  } else {
+    num--;
+    element1.innerText = num;
+    element2.innerText = num;
+    return true;
+  }
+};
+const increment = (event, element1, element2, index) => {
+  event.preventDefault();
+  let num = element1.innerText;
+  const max = index === 1 ? '10' : '30';
+  if (num === max) {
+    return false;
+  } else {
+    num++;
+    element1.innerText = num;
+    element2.innerText = num;
+  }
+};
+
+filterMembers.forEach((item, index) => {
+  document
+    .getElementById(`${item}-minus`)
+    .addEventListener('click', (event) => {
+      decrement(
+        event,
+        document.getElementById(`${item}-amount`),
+        document.getElementById(`${item}-span`),
+        index,
+      );
+    });
+  document.getElementById(`${item}-plus`).addEventListener('click', (event) => {
+    increment(
+      event,
+      document.getElementById(`${item}-amount`),
+      document.getElementById(`${item}-span`),
+      index,
+    );
+  });
 });
