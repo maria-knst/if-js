1c6d36d28caaa7f5aef5bc43844ff1de3819ef19
Maria <konstantinovamaria25@gmail.com>
2022-07-14 13:21:48 +0300

    Added Stack function and tests to it.

---
+++ src/js/stack_les16.js
@@ -0,0 +1,18 @@
+export const checkString = (str) =>{
+ const myStack = [];
+ for(let i = 0; i < str.length; i++){
+   if(str[i] === '{'){
+     myStack.push(str[i]);
+   }else if(str[i] === '}'){
+     if(myStack[myStack.length-1] === '{'){
+       myStack.pop();
+     }
+     else {
+       return false;
+     }
+   }
+ }
+  return myStack.length === 0;
+
+};
+
